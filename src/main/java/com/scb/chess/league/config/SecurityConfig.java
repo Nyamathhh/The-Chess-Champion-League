46a1d293a1bca05bc0c4f7dd0fecad25d4d98217
nyamath <nyamath.shaik@zoom.us>
2024-09-02 13:44:13 +0530

    ignoring CORS

--- src/main/java/com/scb/chess/league/config/SecurityConfig.java
+++ src/main/java/com/scb/chess/league/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -15,13 +17,27 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authz) -> authz
                         .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().permitAll()
-                ).csrf(csrf -> csrf
+                )
+                .csrf(csrf -> csrf
                         .ignoringRequestMatchers("/h2-console/**").disable()
                 )
                 .headers(headers -> headers
                         .frameOptions().sameOrigin()
-                );
+                )
+                .cors();
 
         return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
